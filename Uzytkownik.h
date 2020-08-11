eece1eca696050bb6a9cc049b4f6ea89f71e922a
Joanna Vczynska <joanna.vczynska.programista@gmail.com>
2020-08-11 22:54:56 +0200

    Created base clsses

---
+++ Uzytkownik.h
@@ -0,0 +1,11 @@
+#include <iostream>
+
+using namespace std;
+
+class Uzytkownik
+{
+   public:
+           int id;
+    string login;
+    string haslo;
+};
--- main.cpp
+++ main.cpp
@@ -1,9 +1,9 @@
-#include <iostream>
 
+#include <iostream>
+#include "KsiazkaAdresowa.h"
 using namespace std;
 
 int main()
 {
-    cout << "Hello world!" << endl;
-    return 0;
+KsiazkaAdresowa ksiazkaAdresowa;
 }
